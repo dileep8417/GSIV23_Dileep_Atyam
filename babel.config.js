2026845ade488da27d1231410610c406a6f49a78
dileep <dileep8417@gmail.com>
2023-08-20 08:55:22 +0530

    Configured Webpack and Babel

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+const config = {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['babel-plugin-react-require'],
+};
+
+module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,43 @@
 const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
 
 const config = {
-    entry: {},
-    output: {},
-    optimization: {},
-    module: {},
-    plugins: [],
+    entry: {
+        index: path.resolve('src/index.js'),
+    },
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].js',
+        chunkFilename: 'chunk.[contentHash].[ext]',
+        clean: true,
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        port: 3000
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlPlugin({
+            template: path.resolve('public/index.html'),
+        }),
+    ],
 };
 
 module.exports = config;
