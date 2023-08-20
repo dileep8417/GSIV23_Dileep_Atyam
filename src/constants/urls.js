cdd28f65fec3728debfd57c042c333373020cbdb
dileep <dileep8417@gmail.com>
2023-08-20 12:11:54 +0530

    Added URLS to be used

---
+++ src/constants/urls.js
@@ -0,0 +1,5 @@
+export const movieDiscoverUrl = 'https://api.themoviedb.org/3/discover/movie';
+
+export const movieSearchUrl = 'https://api.themoviedb.org/3/search/movie';
+
+export const movieDetailsUrl = 'https://api.themoviedb.org/3/movie';
