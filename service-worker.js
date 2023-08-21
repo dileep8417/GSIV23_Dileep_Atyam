6e88c42d4d4627cc91d16e5a8e81a9b149a2e866
dileep <dileep8417@gmail.com>
2023-08-21 07:19:14 +0530

    Updated SW

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const cacheName = 'MOVIE_BROWSER';
+const cacheName = 'MOVIE_BROWSER_APP';
 const version = 'v1';
 const currentCache = cacheName + '-' + version;
 const filesToCache = [
     '/',
     'index.html',
     'index.js',
-    'vendors-node_modules_css-loader_dist_runtime_api_js-node_modules_css-loader_dist_runtime_sour-876625.js'
+    'index.css'
 ];
 
 // install
@@ -17,14 +17,12 @@ self.addEventListener('fetch', fetchRequest);
 function installSW(event) {
     event.waitUntil(
         caches.keys()
-            // remove existing caches
             .then(cacheNames => {
                 return Promise.all(
                     cacheNames.filter(cacheName => cacheName !== currentCache)
                     .map(cacheName => caches.delete(cacheName))
                 );
             }).then(() => {
-                // cache files
                 return caches.open(currentCache).then(cache => cache.addAll(filesToCache));
             }).catch(e => console.log(e))
     );
